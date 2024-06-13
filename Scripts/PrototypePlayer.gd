23d09c5a5bccc73e8b4cd95002a432cfab949902
OliverQueenEpicMoments <jguerradominguez@student.neumont.edu>
2024-06-13 03:11:44 -0600

    Player Movement
    Added in sprinting and a stamina system for the player. Additionally added in a prototype enemy.

--- Scripts/PrototypePlayer.gd
+++ Scripts/PrototypePlayer.gd
@@ -1,12 +1,25 @@
 extends CharacterBody2D
 
-
 @export var Speed = 100.0
+@export var SprintSpeed = 200.0
 @export var Acceleration = 10.0
+
+@export var StaminaMax = 100.0
+var StaminaCurrent = 100.0
+@export var StaminaDrainRate = 10.0
+@export var StaminaRegenRate = 5.0
 
 func	_physics_process(delta: float) -> void:
 	var Direction: Vector2 = Input.get_vector("Left", "Right", "Up", "Down")
-	velocity.x = move_toward(velocity.x, Speed * Direction.x, Acceleration)
-	velocity.y = move_toward(velocity.y, Speed * Direction.y, Acceleration)
+	var CurrentSpeed = Speed
+	
+	if Input.is_action_pressed("Sprint") and StaminaCurrent > 0:
+		CurrentSpeed = SprintSpeed
+		StaminaCurrent -= StaminaDrainRate * delta
+	else:
+		StaminaCurrent = clamp(StaminaCurrent, 0, StaminaMax)
+		
+	velocity.x = move_toward(velocity.x, CurrentSpeed * Direction.x, Acceleration)
+	velocity.y = move_toward(velocity.y, CurrentSpeed * Direction.y, Acceleration)
 
 	move_and_slide()
