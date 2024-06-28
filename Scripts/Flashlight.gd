71804c9b4a841ddc5ee6506ed226f07afc83b575
OliverQueenEpicMoments <jguerradominguez@student.neumont.edu>
2024-06-28 01:00:16 -0600

    Flashlight Logic
    The flashlight now works with the UI and works as a toggle.

--- Scripts/Flashlight.gd
+++ Scripts/Flashlight.gd
@@ -1,14 +1,41 @@
 extends Node2D
 
+@export var BatteryMax = 100.0
+var BatteryCurrent = 100.0: set = SetBattery
+@export var BatteryDrainRate = 5.0
+@onready var FlashlightLight = $PointLight2D
+
+var IsActive = false
+
+signal BatteryChanged()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	FlashlightLight.enabled = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+func _process(delta):
+	if (Input.is_action_just_released("Flashlight")):
+		if (IsActive):
+			IsActive = false
+		else: 
+			IsActive = true
+
+			
+	if (IsActive and BatteryCurrent > 0):
+		BatteryCurrent -= BatteryDrainRate * delta
+		FlashlightLight.enabled = true
+	else:
+		FlashlightLight.enabled = false
+
 
 func	_physics_process(_delta: float) -> void:
 	look_at(get_global_mouse_position())
+
+
+func SetBattery(value):
+	var OldValue = BatteryCurrent
+	BatteryCurrent = value
+	BatteryChanged.emit()
--- Scripts/UI/PlayerUI.gd
+++ Scripts/UI/PlayerUI.gd
@@ -2,12 +2,15 @@ extends CanvasLayer
 
 
 @onready var player = %PrototypePlayer
+@onready var Flashlight = %PrototypePlayer/Flashlight
 @onready var StaminaBar = $PlayerInfoBox/StaminaBar
 @onready var FlashlightBar = $PlayerInfoBox/FlashlightBar
 
 func _ready():
 	player.StaminaChanged.connect(Update)
+	Flashlight.BatteryChanged.connect(Update)
 	Update()
 
 func Update():
 	StaminaBar.value = player.StaminaCurrent
+	FlashlightBar.value = Flashlight.BatteryCurrent
